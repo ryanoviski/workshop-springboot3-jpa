9a180c84f00bc86b4d61e16ae1e3c25ef21af2ca
Ryan <ryanoviski@gmail.com>
2025-03-27 18:52:10 -0300

    Payment, one-to-one association

--- src/main/java/com/ryanoviski/course/config/TestConfig.java
+++ src/main/java/com/ryanoviski/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.ryanoviski.course.entities.Category;
 import com.ryanoviski.course.entities.Order;
 import com.ryanoviski.course.entities.OrderItem;
+import com.ryanoviski.course.entities.Payment;
 import com.ryanoviski.course.entities.Product;
 import com.ryanoviski.course.entities.User;
 import com.ryanoviski.course.entities.enums.OrderStatus;
@@ -83,5 +84,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}	
 }
